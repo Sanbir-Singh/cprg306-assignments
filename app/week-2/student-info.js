c166fbf57ad3493e5791ef7637282e40f5f4f14f
Sanbir-Singh <sanbirsingh@edu.sait.ca>
2025-09-11 09:06:20 -0600

    week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Sanbir Singh</h1>
+      <Link href="https://github.com/Sanbir-Singh">
+        https://github.com/Sanbir-Singh
+      </Link>
+    </main>
+  );
+}
